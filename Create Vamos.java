9d7f95053fd2ab210bc7f8b7e3da7ebd5250677a
jcgnzl <jgonmon0107@g.educaand.es>
2024-09-27 18:26:52 +0200

    subiendo cambios

--- Create Vamos.java
+++ Create Vamos.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
   public static void main (String[] args) {
-    System.out.println("VAMOS LINARES OSTIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!");
+    System.out.println("Viernes de guateke");
+	System.out.println("VAMOS LINARES OSTIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!");
   }
 }
